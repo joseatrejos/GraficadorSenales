9c4dffc92ba057838c04710c58420fd654b9808d
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-20 09:44:22 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorSeñales/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraficadorSeñales
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SeñalSenoidal señal = new SeñalSenoidal();
+
+            for (double i = 0; i <= 1; i += 0.0001)
+            {
+                Console.WriteLine(señal.evaluar(i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
